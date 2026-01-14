d9231e18fa5c6d6189465fe8e17eed95d9fe2d8d
Bob DeRosa <4760214+chefbob@users.noreply.github.com>
2026-01-14 13:22:57 -0500

    Initial Commit

---
+++ OOMCP/OOMCPApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOMCPApp.swift
+//  OOMCP
+//
+//  Created by Robert DeRosa on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct OOMCPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OOMCPTests/OOMCPTests.swift
@@ -0,0 +1,17 @@
+//
+//  OOMCPTests.swift
+//  OOMCPTests
+//
+//  Created by Robert DeRosa on 1/14/26.
+//
+
+import Testing
+@testable import OOMCP
+
+struct OOMCPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
